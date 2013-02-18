1206fd77829ff4ab1301a1a4eccf6ed68309a0d6
Kapil Easwar <bigpopakap@gmail.com>
2013-02-18 09:04:54 -0800

    updated app conf to reference new global file name, and modified 1st evolution script

--- db/evolutions/1.sql
+++ db/evolutions/1.sql
@@ -1,15 +1,21 @@
 # --- !Ups
-CREATE TYPE pk_t AS INTEGER;
-CREATE TYPE user_username_t AS VARCHAR(60);
-CREATE TYPE user_name_t AS VARCHAR(60);
-CREATE TYPE email_t AS VARCHAR(60);
-CREATE TYPE facebook_id_t AS VARCHAR(60);
-CREATE TYPE facebook_token_t VARCHAR(60);
+# TODO define constants for max lengths of fields
+
+CREATE TYPE pk_t AS INTEGER; # TODO use a different type?
+CREATE TYPE user_username_t AS VARCHAR(80);
+CREATE TYPE user_name_t AS VARCHAR(80);
+CREATE TYPE email_t AS VARCHAR(255);
+CREATE TYPE facebook_id_t AS VARCHAR(80);
+CREATE TYPE facebook_token_t VARCHAR(80);
+CREATE TYPE facebook_auth_state_t AS INTEGER; # TODO use smaller integer?
+
+# TODO define constants for 3 fbauthStage values
 
 CREATE TABLE User (
 	pk pk_t PRIMARY KEY,
 	username user_username_t NOT NULL,
 	fbid facebook_id_t NOT NULL UNIQUE,
+	fbauthStage facebook_auth_state_t NOT NULL,
 	firstName user_name_t NOT NULL,
 	lastName user_name_t NOT NULL,
 	email email_t NOT NULL,
@@ -25,12 +31,13 @@ CREATE TABLE Session (
 );
 
 # --- !Downs
-DROP TYPE pk_t;
-DROP TYPE user_username_t;
-DROP TYPE user_name_t;
-DROP TYPE email_t;
-DROP TYPE facebook_id_t;
-DROP TYPE facebook_token_t;
+DROP TYPE IF EXISTS pk_t;
+DROP TYPE IF EXISTS user_username_t;
+DROP TYPE IF EXISTS user_name_t;
+DROP TYPE IF EXISTS email_t;
+DROP TYPE IF EXISTS facebook_id_t;
+DROP TYPE IF EXISTS facebook_token_t;
+DROP TYPE IF EXISTS facebook_auth_state_t;
 
-DROP TABLE Session;
-DROP TABLE User;
+DROP TABLE IF EXISTS Session;
+DROP TABLE IF EXISTS User;
