2a3352ca47562e2c5108a8ebe64afb8e8002a4c8
Kapil Easwar <bigpopakap@gmail.com>
2013-03-02 14:53:14 -0800

    added facebook api class to be filled in

---
+++ app/api/FbApi.java
@@ -0,0 +1,14 @@
+package api;
+
+/**
+ * This class implements interactions with the Facebook REST API
+ * 
+ * @author bigpopakap@gmail.com
+ * @since 2013-03-02
+ *
+ */
+public abstract class FbApi {
+	
+	private static final String GRAPH_API_BASE_URL = "https://graph.facebook.com/";
+
+}
