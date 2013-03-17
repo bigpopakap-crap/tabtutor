faf1e92aa9484ea2282e2486101872cd6ee4c1e5
Kapil Easwar <bigpopakap@gmail.com>
2013-03-17 11:28:13 -0700

    added BaseApiJsonReponse class for apis that expect JSON responses

--- app/api/fb/FbJsonResponse.java
+++ app/api/fb/FbJsonResponse.java
@@ -2,12 +2,9 @@ package api.fb;
 
 import java.util.Map;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.node.JsonNodeFactory;
-
 import play.libs.WS.Response;
 import types.HttpMethodType;
-import api.BaseApiResponse;
+import api.BaseApiJsonResponse;
 
 /**
  * Wraps a JSON response from Facebook and provides some helper methods to access
@@ -17,38 +14,20 @@ import api.BaseApiResponse;
  * @since 2013-03-02
  *
  */
-public class FbJsonResponse extends BaseApiResponse<FbErrorResponseException> {
+public class FbJsonResponse extends BaseApiJsonResponse<FbErrorResponseException> {
 	
 	private final String accessToken; //the access token used for the API call
-	private final JsonNode json; //the actual JSON response
 	
 	public FbJsonResponse(HttpMethodType method, String urlDomain, String urlPath, Map<String, String> params, Response resp, String accessToken) {
 		super(method, urlDomain, urlPath, params, resp);
 		
 		if (accessToken == null) throw new IllegalArgumentException("AccessToken cannot be null");
-		
-		this.json = resp.asJson();
 		this.accessToken = accessToken;
 	}
 	
 	/** Gets the access token used in the request that produced this result */
 	public String getToken() {
 		return accessToken;
-	}
-	
-	/** Gets the JSON response itself */
-	public JsonNode getJson() {
-		return json;
-	}
-	
-	/** Sugar method for testing whether this response was returned from any
-	 *  one of a given set of API paths */
-	public boolean is(String... paths) {
-		if (paths == null) throw new IllegalArgumentException("Paths cannot be null");
-		for (String path : paths) {
-			if (getUrlPath().equals(path)) return true;
-		}
-		return false;
 	}
 	
 	/* *********************************************************
@@ -58,7 +37,7 @@ public class FbJsonResponse extends BaseApiResponse<FbErrorResponseException> {
 	/** Returns true if this response represents an error */
 	@Override
 	public boolean isError() {
-		return !json.path("error").isMissingNode();
+		return !getJson().path("error").isMissingNode();
 	}
 	
 	@Override
@@ -93,22 +72,6 @@ public class FbJsonResponse extends BaseApiResponse<FbErrorResponseException> {
 	/* *********************************************************
 	 *  BEGIN METHODS TO EXTRACT INFO FROM VARIOUS API CALL RESPONSES
 	 ********************************************************* */
-	
-	/** Helper to find a value from the JSON if it originated from one of the
-	 *  given API paths, or throw an error if it did not come from one of those paths */
-	private JsonNode find(String jsonPath, String... acceptableUrlPaths) {
-		if (!is(acceptableUrlPaths)) {
-			throw new UnsupportedOperationException("Getting this field is not supported for these API url paths");
-		}
-		else if (isError()) {
-			//don't throw an error here, just return no data
-			//callers *should* be checking for errors before this anyways
-			return JsonNodeFactory.instance.nullNode();
-		}
-		else {
-			return json.path(jsonPath);
-		}
-	}
 	
 	/** Get the Facebook ID if this response came from the /me path */
 	public String fbId() {
