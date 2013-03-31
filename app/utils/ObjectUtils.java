674beefe74853399431908277b91b80bd0bec6a0
Kapil Easwar <bigpopakap@gmail.com>
2013-03-31 09:39:07 -0700

    Added and used RequestStats

---
+++ app/utils/ObjectUtils.java
@@ -0,0 +1,43 @@
+package utils;
+
+import java.lang.reflect.Field;
+
+/**
+ * Utils for dealing with objects
+ * 
+ * @author bigpopakap
+ * @since 2013-03-31
+ *
+ */
+public class ObjectUtils {
+	
+	public static String getFieldsToString(Object o) {
+		if (o == null) return "null";
+		
+		StringBuilder str = new StringBuilder();
+		str.append(o.getClass()).append(":[\n");
+		
+		//iterate over columns
+		for (Field field : o.getClass().getDeclaredFields()) {
+			String value = null;
+			boolean valueIsException = false;
+			try {
+				value = field.get(o).toString();
+			}
+			catch (Exception ex) {
+				value = ex.getClass().getName();
+				valueIsException = true;
+			}
+			
+			str.append("\t")
+				.append(field.getName())
+				.append(valueIsException ? " threw " : " = ")
+				.append(value)
+				.append("\n");
+		}
+		
+		str.append("]");
+		return str.toString();
+	}
+
+}
