b0db7a03d5f793c3c7b047a399824d845af9f487
Kapil Easwar <bigpopakap@gmail.com>
2013-04-05 07:45:04 -0700

    Simple transaction action

--- app/actions/TransactionAction.java
+++ app/actions/TransactionAction.java
@@ -4,6 +4,10 @@ import play.mvc.Http.Context;
 import play.mvc.Result;
 import actions.ActionAnnotations.Transactioned;
 
+import com.avaje.ebean.Ebean;
+
+import contexts.AppContext;
+
 /**
  * Action for creating and managing a transaction for the duration of the request
  * 
@@ -15,8 +19,25 @@ public class TransactionAction extends BaseAction<Transactioned> {
 
 	@Override
 	protected Result hook_call(Context ctx) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO make it possible to get an extra transaction (either nested, or separate)
+		//TODO allow for transactions across multiple requests
+		boolean successful = false;
+		try {
+			Ebean.beginTransaction();
+			
+			Result result = delegate.call(ctx);
+			successful = true;
+			return result;
+		}
+		finally {
+			//if running tests, don't commit
+			if (AppContext.Mode.isRunningTests() || !successful) {
+				Ebean.rollbackTransaction();
+			}
+			else {
+				Ebean.commitTransaction();
+			}
+		}
 	}
 
 }
