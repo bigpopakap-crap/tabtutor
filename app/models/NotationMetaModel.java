37d09639d3f3283654e4e75dbb0006ab4b1ab78e
Kapil Easwar <bigpopakap@gmail.com>
2013-04-07 17:57:37 -0700

    add serial id

--- app/models/NotationMetaModel.java
+++ app/models/NotationMetaModel.java
@@ -2,8 +2,14 @@ package models;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import types.objects.InstrumentType;
+import types.objects.NotationType;
+import types.objects.SkillLevelType;
 
 /**
 * This Ebean maps to the Staff table, and represents staff metadata
@@ -16,13 +22,22 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "NotationMeta")
 public class NotationMetaModel extends BaseModel {
+	
+	private static final long serialVersionUID = 4536159940032586861L;
 
 	/* **************************************************************************
 	 *  FIELDS
 	 ************************************************************************** */
 	
-	//TODO
-	//TODO use proper foreign object reference for song
+	@Column(name = "pk") @Id public UUID pk;
+	@Column(name = "songPk") public UUID songPk; //TODO use proper foreign object reference
+	@Column(name = "userPk") public UUID author_userPk; //TODO use proper foreign object reference
+	@Column(name = "instrumentType") public InstrumentType instrumentType;
+	@Column(name = "skillLevelType") public SkillLevelType skillLevelType;
+	@Column(name = "notationType") public NotationType notationType;
+	@Column(name = "rating") public double rating;
+	//TODO add reference to the actual notation data
+	//TODO use proper foreign object reference for instrument type list
 	
 	/** Private helper for DB interaction implementation */
 	private static final Finder<UUID, NotationMetaModel> FINDER = new Finder<>(
