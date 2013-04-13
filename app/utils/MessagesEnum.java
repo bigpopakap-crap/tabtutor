4b406cb0bbb257e0ab5aab50218ee14821626308
Kapil Easwar <bigpopakap@gmail.com>
2013-04-12 19:47:13 -0700

    Use messages for the "login" word

--- app/utils/MessagesEnum.java
+++ app/utils/MessagesEnum.java
@@ -20,6 +20,7 @@ public enum MessagesEnum {
 	//TODO define debug language that adds a bunch of stars before and after the string
 	
 	word_user("word_user"),
+	word_login("word_login"),
 	errorPage_title("errorPage_title"),
 	errorPage_internalServerErrorDescription("errorPage_internalServerErrorDescription"),
 	errorPage_pageNotFoundDescription("errorPage_pageNotFoundDescription"),
